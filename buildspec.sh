6ccd5f9c2c865a07eae22959954c1eb9b62b918d
anderovsk <anderovsk@pop-os.localdomain>
2023-04-09 22:42:36 -0300

    Test buildspec

---
+++ buildspec.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [[ "$CODEBUILD_WEBHOOK_HEAD_REF" == refs/tags/* ]];
+then
+    echo "Deploy Production"
+    serverless deploy --stage prod
+elif [ "$CODEBUILD_WEBHOOK_HEAD_REF" = "refs/heads/develop" ]
+then
+    echo "Deploy Development"
+    serverless deploy --stage dev
+elif [ "$CODEBUILD_WEBHOOK_HEAD_REF" = "refs/heads/main" ]
+then
+    echo "Deploy Staging"
+    serverless deploy --stage stg
+else echo "demo"
+fi
